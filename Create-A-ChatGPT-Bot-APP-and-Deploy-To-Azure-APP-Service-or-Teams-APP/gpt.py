c895c9d6bbd31587b105a6d84161448bc5801335
Teo Ma <teo_matt@msn.com>
2023-03-15 21:28:21 +0800

    update chatgpt api to turbo

--- Create-A-ChatGPT-Bot-APP-and-Deploy-To-Azure-APP-Service-or-Teams-APP/gpt.py
+++ Create-A-ChatGPT-Bot-APP-and-Deploy-To-Azure-APP-Service-or-Teams-APP/gpt.py
@@ -1,17 +1,26 @@
+import os
 import openai
 
 def process_gpt(request):
   message = request.form['message']
-  openai.api_key = "<YOUR_API_KEY>"
-  response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=message,
-    max_tokens=1024,
-    temperature=0.5,
+  openai.api_key = "<your openai api key>"
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": message},
+        {"role": "user", "content": message}
+
+    ],
+  
+   # prompt=message,
+   max_tokens=4000,
+   stream=True,
+   temperature=0.3,
   )
 
   # Format the response text
-  response_text = response.choices[0].text.strip()  # Remove leading and trailing whitespace
+  
+  response_text = response.choices[0].message.content.strip()  # Remove leading and trailing whitespace
   response_text = response_text.replace("\n", "<br>")  # Replace newlines with line breaks
   # Return the formatted response text
   return response_text
